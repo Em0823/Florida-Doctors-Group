1107c92b8e443732ebff307ea16a496a29d8bee3
Lorena Miranda <lorenamlr@yahoo.com>
2023-07-18 16:30:05 -0400

    updated services with tab and services principal to 4 items row

--- script.js
+++ script.js
@@ -22,3 +22,42 @@ $(document).ready(function () {
         // Add additional form validation as needed
     });
 });
+
+
+
+//Menu
+
+var navLinks = document.getElementById("navLinks");
+function showMenu(){
+    navLinks.style.right = "0";
+        }
+        function hideMenu(){
+            navLinks.style.right = "-200px";
+        } 
+        
+
+
+        // tab for services
+
+        
+function showTabContent(index) {
+var tabs = document.getElementsByClassName("tab");
+var tabContent = document.getElementsByClassName("tab-pane");
+
+// Hide all tab content
+for (var i = 0; i < tabContent.length; i++) {
+tabContent[i].classList.remove("active");
+}
+
+// Deactivate all tabs
+for (var i = 0; i < tabs.length; i++) {
+tabs[i].classList.remove("active");
+}
+
+// Show selected tab content
+tabContent[index].classList.add("active");
+
+// Activate selected tab
+tabs[index].classList.add("active");
+}
+
